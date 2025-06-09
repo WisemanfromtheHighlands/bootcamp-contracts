35b9ad4b0bbd812b6a5b4fff8938070378ee5107
Jay Bland <jamisonblizzard@gmail.com>
2025-06-08 23:12:18 -0400

    Tenderly Setup2

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,13 +8,13 @@ const COLLECTOR_PRIVATE_KEY = vars.get("COLLECTOR_PRIVATE_KEY")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  networks:{
-    tenderly:{
+  networks: {
+    tenderly: {
       url:`https://virtual.mainnet.rpc.tenderly.co/${TENDERLY_API_KEY}`,
-      accounts:[
-      DEPLOYER_PRIVATE_KEY,
-      COLLECTOR_PRIVATE_KEY
-      ]
+      accounts: [
+        DEPLOYER_PRIVATE_KEY,
+        COLLECTOR_PRIVATE_KEY
+      ]           
     }
   }
 };
